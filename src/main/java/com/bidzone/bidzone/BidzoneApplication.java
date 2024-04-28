e0afa7c785c825c348dc88b6598b07a1eae492a9
Randil Hasanga <randil_2020690@fot.ruh.ac.lk>
2024-04-28 17:00:26 +0530

    init commit

---
+++ src/main/java/com/bidzone/bidzone/BidzoneApplication.java
@@ -0,0 +1,13 @@
+package com.bidzone.bidzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidzoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bidzone/bidzone/BidzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bidzone.bidzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
